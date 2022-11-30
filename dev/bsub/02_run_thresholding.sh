790d85e0300df2a4e9ad6eff25579b51472c266f
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2022-11-30 09:59:34 -0500

    Obviate queue in bsub

--- dev/bsub/02_run_thresholding.sh
+++ dev/bsub/02_run_thresholding.sh
@@ -1,10 +1,10 @@
 cd $(dirname $0)/../..
 
-bsub -J 02_thresholding_2d -q taki_normal -R "rusage[mem=500000]" -o logs/02_thresholding_2d.log -e logs/02_thresholding_2d.log \
+bsub -J 02_thresholding_2d -R "rusage[mem=500000]" -o logs/02_thresholding_2d.log -e logs/02_thresholding_2d.log \
     Rscript dev/cli/02_thresholding.R --run2d
 
-bsub -J 02_thresholding_3d -q taki_normal -R "rusage[mem=500000]" -n 201 -o logs/02_thresholding_3d.log -e logs/02_thresholding_3d.log \
+bsub -J 02_thresholding_3d -R "rusage[mem=500000]" -n 201 -o logs/02_thresholding_3d.log -e logs/02_thresholding_3d.log \
     Rscript dev/cli/02_thresholding.R --run3d
 
-bsub -J 02_thresholding_4d -q taki_normal -R "rusage[mem=500000]" -n 201 -o logs/02_thresholding_4d.log -e logs/02_thresholding_4d.log \
+bsub -J 02_thresholding_4d -R "rusage[mem=500000]" -n 201 -o logs/02_thresholding_4d.log -e logs/02_thresholding_4d.log \
     Rscript dev/cli/02_thresholding.R --run4d
--- dev/bsub/03_run_median_filtering.sh
+++ dev/bsub/03_run_median_filtering.sh
@@ -1,7 +1,7 @@
 cd $(dirname $0)/../..
 
-bsub -J 03_median_filtering_3d -q taki_normal -R "rusage[mem=500000]" -o logs/03_median_filtering_3d.log -e logs/03_median_filtering_3d.log \
+bsub -J 03_median_filtering_3d -R "rusage[mem=500000]" -o logs/03_median_filtering_3d.log -e logs/03_median_filtering_3d.log \
     Rscript dev/cli/03_median_filtering.R --run3d
 
-bsub -J 03_median_filtering_4d  -q taki_normal -R "rusage[mem=500000]" -n 12 -o logs/03_median_filtering_4d.log -e logs/03_median_filtering_4d.log \
+bsub -J 03_median_filtering_4d -R "rusage[mem=500000]" -n 12 -o logs/03_median_filtering_4d.log -e logs/03_median_filtering_4d.log \
     Rscript dev/cli/03_median_filtering.R --run4d
