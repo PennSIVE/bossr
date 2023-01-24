36d3c0d3a019cec4d1f6103a9d068ee55d51e4a3
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2023-01-24 09:44:27 -0500

    fix bugs in components.R
    renamed some functions, fixed bugs

--- R/components.R
+++ R/components.R
@@ -16,7 +16,7 @@ last_ind <- function(arr, n){
     do.call(`[`, c(list(arr), inds))
 }
 
-#' Get adjacent values
+#' Gets k-long values
 #'
 #' The function returns values that span at least k cells over last dimension of the array
 #'
@@ -24,12 +24,12 @@ last_ind <- function(arr, n){
 #' @param k minimum number of adjacent values
 #' @return unique values that span at least k cells over last dimension of the array
 #' @examples
-#' get_adjacent(arr, 2)
-get_adjacent <- function(arr, k){
-    #' returns values that span at least k cells over `d`, i.e. last dim. of arr
+#' get_k_long_labels(arr, 2)
+get_k_long_labels <- function(arr, k){
+
     dlength <- rev(dim(arr))[1]
     # for each elem. in d 
-    adj.vals <- purrr::map(1:dlength, ~unique(as.vector(last_ind(arr, .x)))) |> # list uniques at each t
+    adj.vals <- purrr::map(1:dlength, ~unique(as.vector(last_ind(arr, .x)))) |> # list uniques at each t (or z)
         slider::slide(~purrr::reduce(.x, intersect), .after = k - 1) |> # take intersects in rolling windows of size `k - 1`
         purrr::reduce(c) |> # concatenate 
         unique() |> 
@@ -46,7 +46,7 @@ get_adjacent <- function(arr, k){
 #' @return sum of elements in array that match label
 #' @examples
 #' get_size(arr, 10)
-get_size <- function(arr, label) data.frame(sum(arr == label))
+get_size <- function(arr, label) data.frame(sum(arr == label, na.rm = TRUE))
 
 #'Get centroid of a value in an array
 #'
@@ -74,12 +74,10 @@ get_centroid <- function(arr, label){
 #' @examples
 #' make_cell_df(arr, 10)
 make_cell_df <- function(arr, label) {
-    if(label)
     list(label, get_size(arr, label), get_centroid(arr, label)) |> 
     purrr::reduce(c) |>
     as.data.frame() |>
-    setNames(c('index','size', 'x', 'y', 'z')) #|>
-    #dplyr::filter(size > size.thr)
+    setNames(c('index','size', 'x', 'y', 'z')) 
 }
 
 #' Connected components in an array
@@ -112,23 +110,25 @@ connect.components <- function(roi_mask, r = 21){
 #' track.components(arr, 2, 10)
 #' @export
 track.components <- function(roi_labels, k = 2, size.thr = 10){
-    # 1. subset array based on Z <- should this be done outside the function?
-    # 2. split into list based on t
-    # 3. take unique for each split (map)
-    # 4. slide over window of 2 and reduce(intersection) -> unique.value
-    # 5. save unique values 
-    T <- dim(roi_labels)[4]
-    Z <- dim(roi_labels)[3]
-
-    # TODO: abstract pattern for z or t
-    T.adj.vals <- get_adjacent(roi_labels, k)
-    cell_df <- purrr::map(1:T, ~ get_adjacent(roi_labels[,,,.x], k)) |>
-        purrr::map(~intersect(.x, T.adj.vals)) |>
-        purrr::imap(~ if(length(.x) != 0) make_cell_df(roi_labels[,,,.y], .x) |> cbind(t = .y)) |> 
-        dplyr::bind_rows() |>
-        dplyr::filter(size > size.thr)
-    
-    cell_df
+  # 1. subset array based on Z <- should this be done outside the function?
+  # 2. split into list based on t
+  # 3. take unique for each split (map)
+  # 4. slide over window of 2 and reduce(intersection) -> unique.value
+  # 5. save unique values 
+  T <- dim(roi_labels)[4]
+  Z <- dim(roi_labels)[3]
+  
+  # return labels with span >= k in T dimension
+  T.adj.vals <- get_k_long_labels(roi_labels, k)
+  cell_df <- purrr::map(1:T, ~ get_k_long_labels(roi_labels[,,,.x], k)) |> # at each T, get labels that span at least k-long in Z dimension
+    # choose labels with sufficient span on both T and Z dimension. returns list where index are 1:T
+    purrr::map(~intersect(.x, T.adj.vals)) |> 
+    # for label (i) in timepoint (.y) get cell dimensions
+    purrr::imap(~ if(length(.x) != 0) purrr::map(.x, function (i) make_cell_df(roi_labels[,,,.y], i) |> cbind(t = .y))) |> # .x is an intermediate vector that
+    dplyr::bind_rows() |>
+    dplyr::filter(size > size.thr)
+  
+  cell_df
 }
 
 #' Get number of connected components in each timepoint of 4D array
@@ -163,28 +163,28 @@ get.delta.n <- function(cell_df){
 #'
 #' This function performs annotation on a data frame by sorting unique indexes and then filtering the dataframe based on X, Y, and Z coordinates. The function then checks for certain conditions and assigns new indexes to rows that meet those conditions.
 #'
-#' @param dat dataframe
+#' @param df dataframe
 #' @param t 
 #' @return the annotated dataframe
 #' @export
-annot.dat <- function(df, t){
+annotate.df <- function(df, t){
     index <- sort(unique(df$index))
   
     result.index <- c()
     for(i in index){
-      index.X <- dat[which(df$index==i),"X"]
-      index.Y <- dat[which(df$index==i),"Y"]
-      index.Z <- dat[which(df$index==i),"Z"]
+      index.X <- df[which(df$index==i),"x"]
+      index.Y <- df[which(df$index==i),"y"]
+      index.Z <- df[which(df$index==i),"z"]
 
-      new.df <- df %>% filter(X <= max(index.X)+30 & X >= min(index.X)-30) %>% 
-        filter(Y <= max(index.Y)+30 & Y >= min(index.Y)-30) %>%
-        filter(Z <= max(index.Z)+5 & Z >= min(index.Z)-5) 
+      new.df <- df |> dplyr::filter(x <= max(index.X)+30 & x>= min(index.X)-30) |> 
+        dplyr::filter(Y <= max(index.Y)+30 & Y >= min(index.Y)-30) |>
+        dplyr::filter(Z <= max(index.Z)+5 & Z >= min(index.Z)-5) 
 
       if(length(unique(new.df$index)) >1 && length(unique(new.df$T)) == nrow(new.df)){
         print(new.df)
         index.t <- c()
         for(j in unique(new.df$index)){
-          index.t <- c(index.t, df[which(df$index == j),"T"])
+          index.t <- c(index.t, df[which(df$index == j),"t"])
         }
 
         if(length(index.t) == length(unique(index.t))){
@@ -197,7 +197,7 @@ annot.dat <- function(df, t){
         print("Good")
       }
     }
-    
+
     result.index <- result.index[which(duplicated(result.index)==FALSE),]
     for(i in 1:nrow(result.index)){
       df$index[which(df$index==result.index[i,2])] <- result.index[i,1]
