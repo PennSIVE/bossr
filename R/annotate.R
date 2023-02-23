5fd3252b9e38104220b86b949faa9db23e33c80f
Virgilio Gonzenbach <62161049+vgonzenbach@users.noreply.github.com>
2023-02-23 18:36:35 -0500

    fix deleted column in annotate_df()
    also tweaked documentation

--- R/annotate.R
+++ R/annotate.R
@@ -1,32 +1,33 @@
 #' Annotate data.frame
 #'
-#' This function takes in a dataframe that is result of post.process.df (or track.components) 
-#' and an integer t (total timepoints), and returns a dataframe with 4 columns: 
-#' `count`, `added`, `deleted`, and `survivor`. `count` is the number of cells at timepoint t. 
-#' `added` is the number of new cells in timepoint t (relative to timepoint t - 1); 
+#' This function takes in a dataframe that is result of post_process_df (or track_components) 
+#' and an integer t (total time points), and returns a dataframe with 4 columns: 
+#' `count`, `added`, `deleted`, and `survivor`. `count` is the number of cells at time point t. 
+#' `added` is the number of new cells in time point t (relative to time point t - 1); 
 #' `added` is always NA for t = 1. 
-#' `deleted` is the number of cells at t that do not show up at later timepoints.
-#' `survivor` is the number of cells at timepoint t that remain from timepoint 1.
+#' `deleted` is the number of cells at t that do not show up at later time points.
+#' `survivor` is the number of cells at time point t that remain from time point 1.
 #'
-#' @param df A dataframe
+#' @param cell_df A dataframe that is the result from pos
 #' @param t An integer value
 #' @return A dataframe with 4 columns: count, addition, deletion, and survivor.
 #' @import dplyr
 #' @export
-annotate_df = function (df, t) {
+annotate_df = function (cell_df, t) {
   
   annot_df <- data.frame(t = seq_len(t)) # declare return object
   
   # get count at each t ----
-  count_df <- dplyr::count(cell_df, t)
+  count_df <- dplyr::count(cell_df, t) |>
+    dplyr::rename(count = n)
   annot_df <- annot_df |> 
     dplyr::full_join(count_df, by = 't')
   
-  # get num added at each t: get indices at timepoint t that are new relative to timepoint t - 1 ----
+  # get num added at each t: get indices at time point t that are new relative to time point t - 1 ----
   added_df <- split(cell_df$index, cell_df$t) |> # get indices at t
     
-    slider::slide(~ setdiff(unlist(.x[2]), # indices at timepoint t (see .before)
-                            unlist(.x[1])) |> # indices at timepoint t - 1
+    slider::slide(~ setdiff(unlist(.x[2]), # indices at time point t (see .before)
+                            unlist(.x[1])) |> # indices at time point t - 1
                     length() |> 
                     as.data.frame() |> 
                     setNames('added'), 
@@ -35,26 +36,26 @@ annotate_df = function (df, t) {
     dplyr::mutate(t = as.integer(t)) # add col for joining
   annot_df <- annot_df |> 
     dplyr::full_join(added_df, by = 't')
-  annot_df[1, 'added'] <- NA # set 0 to NA; timepoint 1 does not have a timepoint before it
+  annot_df[1, 'added'] <- NA # set 0 to NA; time point 1 does not have a time point before it
   
-  # get num deleted: for timepoint t, how many indices at t do not show up later (at T > t) ----
+  # get num deleted: for time point t, how many indices from t-1 are not present anymore----
   del_df <- split(cell_df$index, cell_df$t) |>
-    slider::slide(~ setdiff(unlist(.x[1]), # timepoint t
-                            unlist(.x[-1])) |> # all later timepoints (excluding timepoint t)
+    slider::slide(~ setdiff(unlist(.x[1]), # time point t - 1
+                            unlist(.x[2])) |> # all later time points (excluding time point t)
                     length() |> 
                     as.data.frame() |> 
                     setNames('deleted'), 
-                  .after = Inf) |>
+                  .before = 1) |>
     dplyr::bind_rows(.id = 't') |> 
     dplyr::mutate(t = as.integer(t)) # add col for joining
-  del_df[nrow(del_df), 'deleted'] <- NA
+  del_df[1, 'deleted'] <- NA
   annot_df <- annot_df |>  # join results 
     dplyr::full_join(del_df, by = 't')
   
-  # get survivors: at timepoint t, how many cells remain from timepoint 1
+  # get survivors: at time point t, how many cells remain from time point 1
   surv_df <- split(cell_df$index, cell_df$t) |>
-    slider::slide(~ intersect(unlist(.x[1]), # indices at timepoint 1
-                              unlist(.x[length(.x)])) |> # indices at timepoint t
+    slider::slide(~ intersect(unlist(.x[1]), # indices at time point 1
+                              unlist(.x[length(.x)])) |> # indices at time point t
                     length() |> 
                     as.data.frame() |> 
                     setNames('survivor'),
@@ -63,18 +64,18 @@ annotate_df = function (df, t) {
     dplyr::mutate(t = as.integer(t)) # add col for joining
   annot_df <- annot_df |>  # join results 
     dplyr::full_join(surv_df, by = 't')
-  annot_df[1, 'survivor'] <- NA # redundant to compare timepoint 1 with itself
+  annot_df[1, 'survivor'] <- NA # redundant to compare time point 1 with itself
   
   return(annot_df)
 }
 
 #' Make Overlay
 #' 
-#' This function creates an overlay by drawing boxes around centroids of cells in a dataframe, at a specified timepoint, as well as cells in adjacent timepoints.
+#' This function creates an overlay by drawing boxes around centroids of cells in a dataframe, at a specified time point, as well as cells in adjacent time points.
 #' 
 #' @param cell_df A dataframe containing the cell data, with columns for x, y, z, t, and index
 #' @param dims A numeric vector of length 4, representing the dimensions of the image in x, y, z, and t
-#' @param t An integer representing the timepoint of interest
+#' @param t An integer representing the time point of interest
 #' 
 #' @return A 3-dimensional binary array representing the overlay 
 #' @export
@@ -93,14 +94,14 @@ make_overlay <- function(cell_df, dims, t){
   }
   
   if(! t %in% seq_len(dims[4])) stop("t is outside of the specified dimensions.")
-  ## get cells at timepoint t
+  ## get cells at time point t
   df_t <- cell_df |>
     dplyr::filter(.data$t == t) 
   
   index_t <- unique(df_t$index)
   
-  ## search cells in adjacent timepoints
-  # get adjacent timepoints if they exist (handles edge cases when t=1 and t=T)
+  ## search cells in adjacent time points
+  # get adjacent time points if they exist (handles edge cases when t=1 and t=T)
   t_adj <- intersect(c(t-1, t+1), 
                      seq_len(dims[4]))
   # get the cells at the adj
